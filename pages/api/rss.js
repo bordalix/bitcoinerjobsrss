e651d8bd3c843b5f8a08781a82e3c25ed9752da2
João Bordalo <joao.bordalo@gmail.com>
2021-10-26 17:24:21 +0100

    do not respond in json

--- pages/api/rss.js
+++ pages/api/rss.js
@@ -4,9 +4,9 @@ import { getJobs } from '../../lib/jobs';
 export default async function handler(req, res) {
   try {
     const jobs = await getJobs()
-    res.status(200).json(generateRssFeed(jobs))
+    res.status(200).send(generateRssFeed(jobs))
   } catch(error) {
     console.log(error)
-    res.status(500).json({ error: 'Something went wrong' })
+    res.status(500).send('Something went wrong')
   }
 }
